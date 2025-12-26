102b8ab19b149567557d7798476a820b0742fbe0
Yogesh <90864309+Yogesh4242@users.noreply.github.com>
2025-12-26 06:50:22 +0530

    changing it back

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
